6bac72d1ea1f90e0b135844dc59172075d64ba70
Jussi Utunen <jussi.utunen@gmail.com>
2021-07-25 18:41:08 +0300

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -3,46 +3,61 @@ import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { useState, useEffect } from "react";
-import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
 import fi from 'date-fns/locale/fi';
+import { ResultTable } from "./ResultTable.js";
+
+async function handleDateChange(date, setJsonData, setStartDate) {
+
+  console.log(date.toISOString());
+  setStartDate(date);
+
+  const encodedDate = encodeURIComponent(date.toISOString());
+  //const encodedDate = encodeURIComponent('perseensuti');
+
+  //setRequestIsPending(true);
+  try {
+    const result = await axios.get(`http://localhost:5555/test?date=${encodedDate}`,
+      {
+        timeout: 10000
+      });
+    console.log(result.data);
+    setJsonData(JSON.stringify(result.data));
+  } catch (err) {
+    console.error(err);
+    //alert("Data fetching failed.\nPlease try again later.");
+    //setRequestIsPending(false);
+  }
+  //setRequestIsPending(false);
+
+}
 
 function App() {
 
-  const [startDate, setStartDate] = useState(new Date('2021-01-02'));
-
   useEffect(() => {
     registerLocale('fi', fi);
-  },[]);  
-
-  async function handleDateChange(date) {
-
-    console.log(date.toISOString());
-    setStartDate(date);
-
-    const encodedDate = encodeURIComponent(date.toISOString());
-    //const encodedDate = encodeURIComponent('perseensuti');
-
-    //setRequestIsPending(true);
-    try {
-      const result = await axios.get(`http://localhost:5555/test?date=${encodedDate}`,
-                                      {
-                                        timeout: 10000
-                                      });
-      console.log(result.data);
-    } catch (err) {
-      console.error(err);
-      //alert("Data fetching failed.\nPlease try again later.");
-      //setRequestIsPending(false);
-    }
-    //setRequestIsPending(false);
-
-  }
+  }, []);
 
   return (
-    <div className="App">
+    <div id="main_container">
+      <MainComponent />
+      <MainComponent />
+      <MainComponent />
+    </div>
+
+  );
+}
+
+function MainComponent() {
+
+  const [startDate, setStartDate] = useState(new Date('2021-01-02'));
+  const [jsonData, setJsonData] = useState("{}");
+
+  return (
+    <div className="main_component">
       <DatePicker
         selected={startDate}
-        onChange={(date) => handleDateChange(date)}
+        onChange={(date) => handleDateChange(date, setJsonData, setStartDate)}
         locale="fi"
         showTimeSelect
         timeFormat="p"
@@ -51,8 +66,9 @@ function App() {
         minDate={new Date('2021-01-01')}
         maxDate={new Date('2021-04-15')}
       />
+      <ResultTable results={JSON.parse(jsonData)} />
     </div>
-  );
+  )
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -20,9 +20,6 @@ app.get("/test", async (req, res, next) => {
 
     const dateObj = new Date(dateString);
 
-    //2021-04-13T23:00:00.000Z
-    //2021-04-12 13:10:06
-
     if(isNaN(dateObj.getTime())) {
         return res.status(400).send("date is invalid");
     }
@@ -34,7 +31,7 @@ app.get("/test", async (req, res, next) => {
         return res.status(400).send("date is out of bounds");
     }
 
-    // convert dateString to Postgres date format
+    // convert dateString to Postgres date format:
     const datetime = dateString.replace('T',' ').replace('Z','');
     console.log("datetime: " + datetime);
 
@@ -44,61 +41,31 @@ app.get("/test", async (req, res, next) => {
     };
 
     try {
-        results.usedVaccinations = await getUsedVaccinationsCount(knex, datetime);
-    } catch (error) {
-        return next(error);
-    }
-
-    try {
+        results.usedVaccines = await getUsedVaccinesCount(knex, datetime);
         results.arrivedOrders = await getArrivedOrdersCount(knex, datetime);
-    } catch (error) {
-        return next(error);
-    }
-
-    try {
         results.arrivedVaccines = await getArrivedVaccinesCount(knex, datetime);
-    } catch (error) {
-        return next(error);
-    }
-
-    try {
         results.arrivedOrdersPerProducer[vaccineBrands[0]] = await getArrivedOrdersCountPerProducer(knex, datetime, vaccineBrands[0]);
-    } catch (error) {
-        return next(error);
-    }
-
-    try {
         results.arrivedOrdersPerProducer[vaccineBrands[1]] = await getArrivedOrdersCountPerProducer(knex, datetime, vaccineBrands[1]);
-    } catch (error) {
-        return next(error);
-    }
-
-    try {
         results.arrivedOrdersPerProducer[vaccineBrands[2]] = await getArrivedOrdersCountPerProducer(knex, datetime, vaccineBrands[2]);
-    } catch (error) {
-        return next(error);
-    }
-
-    try {
         results.arrivedVaccinesPerProducer[vaccineBrands[0]] = await getArrivedVaccinesCountPerProducer(knex, datetime, vaccineBrands[0]);
-    } catch (error) {
-        return next(error);
-    }
-
-    try {
         results.arrivedVaccinesPerProducer[vaccineBrands[1]] = await getArrivedVaccinesCountPerProducer(knex, datetime, vaccineBrands[1]);
-    } catch (error) {
-        return next(error);
-    }
-
-    try {
         results.arrivedVaccinesPerProducer[vaccineBrands[2]] = await getArrivedVaccinesCountPerProducer(knex, datetime, vaccineBrands[2]);
+        results.expiredBottles = await getExpiredBottlesCount(knex, datetime);
+        const vaccinesGivenFromExpiredBottles = await getExpiredBottlesGivenVaccinesCount(knex, datetime);
+        const totalVaccinesInExpiredBottles = await getExpiredBottlesTotalVaccinesCount(knex, datetime);
+        results.vaccinesExpiredBeforeUsage = totalVaccinesInExpiredBottles - vaccinesGivenFromExpiredBottles;
+        const vaccinesGivenFromValidBottles = await getValidBottlesGivenVaccinesCount(knex, datetime);
+        console.log("vaccinesGivenFromValidBottles: " + vaccinesGivenFromValidBottles);
+        const totalVaccinesInValidBottles = await getValidBottlesTotalVaccinesCount(knex, datetime);
+        console.log("totalVaccinesInValidBottles: " + totalVaccinesInValidBottles);
+        results.validVaccinesLeft = totalVaccinesInValidBottles - vaccinesGivenFromValidBottles;
+        results.vaccinesExpiringInNext10days = await getExpiringVaccinesCount(knex, datetime, 10);
     } catch (error) {
         return next(error);
     }
 
-    console.log("results:");
-    console.log(JSON.stringify(results));
+    //console.log("results:");
+    //console.log(JSON.stringify(results));
     res.send(results);
 
 });
@@ -106,8 +73,8 @@ app.get("/test", async (req, res, next) => {
 console.log("Listening port 5555");
 app.listen(5555);
 
-// 1. How many of the vaccinations have been used
-const getUsedVaccinationsCount = async (knex, datetime) => {
+// 1. How many of the vaccines have been used
+const getUsedVaccinesCount = async (knex, datetime) => {
     let result = await knex.raw(`select count(*) from vaccination where date <= '${datetime}';`);
     return result.rows[0].count;
 }
@@ -144,36 +111,46 @@ const getExpiredBottlesCount = async (knex, datetime) => {
 
 // 7. How many vaccines expired before the usage -> remember to decrease used injections from the expired bottle
 
-//  7.1 How many vaccinations were given from the bottles that were expired by the given day
-const getExpiredBottlesGivenVaccinationsCount = async (knex, datetime) => {
+//  7.1 How many vaccines were given from the bottles that were expired by the given day
+const getExpiredBottlesGivenVaccinesCount = async (knex, datetime) => {
     let result = await knex.raw(`select count(*) from vaccination where vaccination.bottle in (select id from bottle where expired <= '${datetime}');`);
     return result.rows[0].count;;
 }
 
-// 7.2 How many vaccinations were in the bottles that were expired by the given day
-const getExpiredBottlesTotalVaccinationsCount = async (knex, datetime) => {
+// 7.2 How many vaccines were in the bottles that were expired by the given day
+const getExpiredBottlesTotalVaccinesCount = async (knex, datetime) => {
     let result = await knex.raw(`select sum(injections) as injections_count from bottle where expired <= '${datetime}';`);
     return result.rows[0].injections_count;;
 }
 
 // 8. How many vaccines are left to use?
 
-// 8.1 How many vaccinations have been given from the bottles, that have not expired by the given day
-const getValidBottlesGivenVaccinationsCount = async (knex, datetime) => {
+// 8.1 How many vaccines have been given from the bottles, that have not expired by the given day
+const getValidBottlesGivenVaccinesCount = async (knex, datetime) => {
     let result = await knex.raw(`select count(*) from vaccination where vaccination.bottle in (select id from bottle where expired > '${datetime}');`);
-    return result.rows;
+    return result.rows[0].count;
 }
 
-// 8.2 How many vaccinations has been in the bottles, that have not expired by the given day
-const getValidBottlesTotalVaccinationsCount = async (knex, datetime) => {
+// 8.2 How many vaccines has been in the bottles, that have not expired by the given day
+const getValidBottlesTotalVaccinesCount = async (knex, datetime) => {
     let result = await knex.raw(`select sum(injections) as injections_count from bottle where expired > '${datetime}';`);
-    return result.rows;
+    return result.rows[0].injections_count;
 }
 
 // 9. How many vaccines are going to expire in the next 10 days
 const getExpiringVaccinesCount = async (knex, datetime, periodInDays) => {
-    let result = await knex.raw(`select sum(injections) as injections_count from bottle where expired >= '${datetime}' and expired <= ('${datetime}'::timestamp + '${periodInDays} days'::interval);`);
-    return result.rows;
+
+    let result = await knex.raw(`select count(*) from bottle where expired >= '${datetime}' and expired <= ('${datetime}'::timestamp + '${periodInDays} days'::interval);`);
+
+    //console.log("result.rows[0]");
+    //console.log(JSON.stringify(result.rows[0]));
+
+    if(result.rows[0].count == 0 ) {
+        return 0;
+    }
+    
+    result = await knex.raw(`select sum(injections) as injections_count from bottle where expired >= '${datetime}' and expired <= ('${datetime}'::timestamp + '${periodInDays} days'::interval);`);
+    return result.rows[0].injections_count;
 }
 
 
