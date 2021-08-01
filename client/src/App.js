7e8caf284fc5b7df35a08e91225c155ffe648a2a
Jussi Utunen <jussi.utunen@gmail.com>
2021-08-01 10:36:04 +0300

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,14 @@
 import "./App.css";
 import axios from "axios";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState, useEffect, createRef } from "react";
-import { registerLocale, setDefaultLocale } from "react-datepicker";
+import { useState, useEffect } from "react";
+import { registerLocale  } from "react-datepicker";
 import fi from "date-fns/locale/fi";
-import { ResultTable } from "./ResultTable.js";
-import delButtonPng from "./delete-button.png";
+import { InfoComponent, ProgressIndicator } from "./Components.js";
 import cloneDeep from "lodash.clonedeep";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { Spinner } from "react-spinners-css";
 
 const initDateConst = "2021-01-02T10:00:00Z";
-const minDateConst = 1609545600000; // '2021-01-02T00:00:00.000Z'
-const maxDateConst = 1618257600000; // '2021-04-12T20:00:00.000Z'
 
 function App() {
     useEffect(() => {
@@ -63,13 +58,13 @@ function App() {
         }
     };
 
-    const removeHandler = idToBeRemoved => {
+    const removeItem = idToBeRemoved => {
         if (dataArray.length > 1) {
             setDataArray(dataArray.filter(item => item.id !== idToBeRemoved));
         }
     };
 
-    const duplicatingHandler = (date, data, index) => {
+    const duplicateItem = (date, data, index) => {
         let arrayClone = [];
 
         for (let i = 0, shift = 0; i < dataArray.length; i++) {
@@ -102,8 +97,7 @@ function App() {
     return (
         <div id="main_container">
             <div id="heading">
-              List of interesting things aka Solita vaccine exercise 2021<br />
-              Click duplicate button to compare two or more dates!
+              <div>List of interesting things aka Solita vaccine exercise 2021. Click <span style={{fontStyle:'italic'}}>duplicate</span> button to compare two or more dates!</div>
             </div>
             <div id="components_container">
                 <TransitionGroup component={null}>
@@ -115,11 +109,11 @@ function App() {
                                 key={item.id}
                             >
                                 <InfoComponent
-                                    duplicatingHandler={duplicatingHandler}
+                                    duplicatingHandler={duplicateItem}
                                     count={dataArray.length}
                                     removable={dataArray.length > 1}
                                     id={item.id}
-                                    removeHandler={removeHandler}
+                                    removeHandler={removeItem}
                                     initData={item.data}
                                     initDateStr={item.dateStr}
                                     index={index++}
@@ -131,140 +125,6 @@ function App() {
                 </TransitionGroup>
             </div>
             <ProgressIndicator visible={requestIsPending} />
-        </div>
-    );
-}
-
-function TimeController({ label, shift, changeHandler }) {
-    return (
-        <div className="time_controller">
-            <button
-                type="button"
-                className="time_adjust_button"
-                onClick={() => changeHandler(shift * -1)}
-            >
-                -
-            </button>
-            <div className="time_label">{label}</div>
-            <button
-                type="button"
-                className="time_adjust_button"
-                onClick={() => changeHandler(shift)}
-            >
-                +
-            </button>
-        </div>
-    );
-}
-
-function InfoComponent({
-    id,
-    removeHandler,
-    initData,
-    removable,
-    duplicatingHandler,
-    initDateStr,
-    index,
-    count,
-    getFunction,
-}) {
-    const [startDate, setStartDate] = useState(new Date(initDateStr));
-    const [vaxData, setVaxData] = useState(null);
-
-    const updateTime = timeChangeInMs => {
-        let newTime = startDate.getTime() + timeChangeInMs;
-        if (newTime < minDateConst || newTime > maxDateConst) {
-            return;
-        }
-        getFunction(new Date(newTime), setVaxData, setStartDate);
-    };
-
-    //console.log("initData: ", initData);
-
-    return (
-        <div className="info_component">
-            <div className="info_controls">
-                <div className="info_sub_controls">
-                    <div className="info_sub_sub_controls">
-                        <button
-                            style={{
-                                visibility: count < 5 ? "visible" : "hidden",
-                            }}
-                            type="button"
-                            onClick={() =>
-                                duplicatingHandler(
-                                    startDate.toISOString(),
-                                    !vaxData ? initData : vaxData,
-                                    index
-                                )
-                            }
-                        >
-                            Duplicate
-                        </button>
-                        <input
-                            style={{
-                                visibility: removable ? "visible" : "hidden",
-                            }}
-                            type="image"
-                            src={delButtonPng}
-                            alt="Remove"
-                            height="25"
-                            width="25"
-                            onClick={() => removeHandler(id)}
-                            onMouseEnter={() => {}}
-                            onMouseLeave={() => {}}
-                        />
-                    </div>
-                    Adjust date and time:
-                    <TimeController
-                        label="week"
-                        shift={7 * 24 * 3.6e6}
-                        changeHandler={updateTime}
-                    />
-                    <TimeController
-                        label="day"
-                        shift={24 * 3.6e6}
-                        changeHandler={updateTime}
-                    />
-                    <TimeController
-                        label="hour"
-                        shift={3.6e6}
-                        changeHandler={updateTime}
-                    />
-                </div>
-                Select date and time:
-                <DatePicker
-                    selected={startDate}
-                    onChange={date =>
-                        getFunction(date, setVaxData, setStartDate)
-                    }
-                    showTimeSelect
-                    timeIntervals={30}
-                    dateFormat="dd.MM.yyyy HH:mm"
-                    minDate={new Date(minDateConst)}
-                    maxDate={new Date(maxDateConst)}
-                    className="date_picker"
-                    locale={fi}
-                    calendarStartDay={1}
-                >
-                    <div style={{ marginLeft: "5px", fontSize: "12px" }}>
-                        Calendar times are Finnish time.
-                    </div>
-                </DatePicker>
-            </div>
-            <ResultTable results={!vaxData ? initData : vaxData} />
-        </div>
-    );
-}
-
-function ProgressIndicator({ visible }) {
-    if (!visible) {
-        return null;
-    }
-
-    return (
-        <div style={{ position: "absolute", top: "30%" }}>
-            <Spinner color={"black"} />
         </div>
     );
 }
---
+++ client/src/Components.js
@@ -0,0 +1,224 @@
+import DatePicker from "react-datepicker";
+import "./Components.css";
+import delButtonPng from "./delete-button.png";
+import { Spinner } from "react-spinners-css";
+import { useState } from "react";
+import fi from "date-fns/locale/fi";
+const minDateConst = 1609545600000; // '2021-01-02T00:00:00.000Z'
+const maxDateConst = 1618257600000; // '2021-04-12T20:00:00.000Z'
+const maxNumberOfComponents = 10;
+
+function TimeController({ label, shift, changeHandler }) {
+    return (
+        <div className="time_controller">
+            <button
+                type="button"
+                className="time_adjust_button"
+                onClick={() => changeHandler(shift * -1)}
+            >
+                -
+            </button>
+            <div className="time_label">{label}</div>
+            <button
+                type="button"
+                className="time_adjust_button"
+                onClick={() => changeHandler(shift)}
+            >
+                +
+            </button>
+        </div>
+    );
+}
+
+export function InfoComponent({
+    id,
+    removeHandler,
+    initData,
+    removable,
+    duplicatingHandler,
+    initDateStr,
+    index,
+    count,
+    getFunction,
+}) {
+    const [startDate, setStartDate] = useState(new Date(initDateStr));
+    const [vaxData, setVaxData] = useState(null);
+
+    const updateTime = timeChangeInMs => {
+        let newTime = startDate.getTime() + timeChangeInMs;
+        if (newTime < minDateConst || newTime > maxDateConst) {
+            return;
+        }
+        getFunction(new Date(newTime), setVaxData, setStartDate);
+    };
+
+    //console.log("initData: ", initData);
+
+    return (
+        <div className="info_component">
+            <div className="info_controls">
+                <div className="info_sub_controls">
+                    <div className="info_sub_sub_controls">
+                        <button
+                            style={{
+                                visibility:
+                                    count < maxNumberOfComponents
+                                        ? "visible"
+                                        : "hidden",
+                            }}
+                            type="button"
+                            onClick={() =>
+                                duplicatingHandler(
+                                    startDate.toISOString(),
+                                    !vaxData ? initData : vaxData,
+                                    index
+                                )
+                            }
+                        >
+                            Duplicate
+                        </button>
+                        <input
+                            style={{
+                                visibility: removable ? "visible" : "hidden",
+                            }}
+                            type="image"
+                            src={delButtonPng}
+                            alt="Remove"
+                            height="25"
+                            width="25"
+                            onClick={() => removeHandler(id)}
+                            onMouseEnter={() => {}}
+                            onMouseLeave={() => {}}
+                        />
+                    </div>
+                    Adjust date and time:
+                    <TimeController
+                        label="week"
+                        shift={7 * 24 * 3.6e6}
+                        changeHandler={updateTime}
+                    />
+                    <TimeController
+                        label="day"
+                        shift={24 * 3.6e6}
+                        changeHandler={updateTime}
+                    />
+                    <TimeController
+                        label="hour"
+                        shift={3.6e6}
+                        changeHandler={updateTime}
+                    />
+                </div>
+                Select date and time:
+                <DatePicker
+                    selected={startDate}
+                    onChange={date =>
+                        getFunction(date, setVaxData, setStartDate)
+                    }
+                    showTimeSelect
+                    timeIntervals={30}
+                    dateFormat="dd.MM.yyyy HH:mm"
+                    minDate={new Date(minDateConst)}
+                    maxDate={new Date(maxDateConst)}
+                    className="date_picker"
+                    locale={fi}
+                    calendarStartDay={1}
+                >
+                    <div style={{ marginLeft: "5px", fontSize: "12px" }}>
+                        Calendar times are Finnish time.
+                    </div>
+                </DatePicker>
+            </div>
+            <ResultTable results={!vaxData ? initData : vaxData} />
+        </div>
+    );
+}
+
+export function ProgressIndicator({ visible }) {
+    if (!visible) {
+        return null;
+    }
+
+    return (
+        <div style={{ position: "absolute", top: "30%" }}>
+            <Spinner color={"black"} />
+        </div>
+    );
+}
+
+function ResultTable({ results }) {
+    if (Object.keys(results).length === 0) {
+        return null;
+    }
+
+    return (
+        <div className="result_table">
+            <div className="result_item">
+                <div className="result_title">Bottles arrived:</div>
+                <div className="result_value">{results.arrivedOrders}</div>
+            </div>
+
+            <div className="result_item">
+                <div className="result_title">Vaccines arrived:</div>
+                <div className="result_value">{results.arrivedVaccines}</div>
+            </div>
+
+            <div className="result_item">
+                <div className="result_title">Vaccines used:</div>
+                <div className="result_value">{results.usedVaccines}</div>
+            </div>
+
+            <div className="result_item">
+                <div className="result_title">Antiqua arrivals:</div>
+                <div className="result_value">
+                    {results.arrivedOrdersPerProducer.Antiqua} bottles <br />
+                    {results.arrivedVaccinesPerProducer.Antiqua} vaccines
+                </div>
+            </div>
+
+            <div className="result_item">
+                <div className="result_title">SolarBuddhica arrivals:</div>
+                <div className="result_value">
+                    {results.arrivedOrdersPerProducer.SolarBuddhica} bottles{" "}
+                    <br />
+                    {results.arrivedVaccinesPerProducer.SolarBuddhica} vaccines
+                </div>
+            </div>
+
+            <div className="result_item">
+                <div className="result_title">Zerpfy arrivals:</div>
+                <div className="result_value">
+                    {results.arrivedOrdersPerProducer.Zerpfy} bottles <br />
+                    {results.arrivedVaccinesPerProducer.Zerpfy} vaccines
+                </div>
+            </div>
+
+            <div className="result_item">
+                <div className="result_title">Bottles expired:</div>
+                <div className="result_value">{results.expiredBottles}</div>
+            </div>
+
+            <div className="result_item">
+                <div className="result_title">
+                    Vaccines expired before usage:
+                </div>
+                <div className="result_value">
+                    {results.vaccinesExpiredBeforeUsage}
+                </div>
+            </div>
+
+            <div className="result_item">
+                <div className="result_title">Vaccines left to be used:</div>
+                <div className="result_value">{results.validVaccinesLeft}</div>
+            </div>
+
+            <div className="result_item">
+                <div className="result_title">
+                    Vaccines expiring in next 10 days:
+                </div>
+                <div className="result_value">
+                    {results.vaccinesExpiringInNext10days}
+                </div>
+            </div>
+        </div>
+    );
+}
